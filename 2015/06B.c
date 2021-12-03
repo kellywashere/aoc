fe4bf850243bca8e941616eb6c6052b4ea43d27e
Remco van de Beek <remco.van.de.beek@nxp.com>
2021-12-03 14:49:12 +0100

    Solved 06

---
+++ 2015/06B.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define GRIDSIZE 1000
+
+enum command {
+	ON,
+	OFF,
+	TOGGLE
+};
+
+void parse_coordinate(const char* line, int* idx, int* x, int* y) {
+	char* endptr;
+	*x = (int)strtol(line + *idx, &endptr, 10);
+	if (*endptr != ',')
+		fprintf(stderr, "Did not see , in coordinates: %s\n", line);
+	*y = (int)strtol(endptr + 1, &endptr, 10);
+	*idx = endptr - line;
+}
+
+int main(int argc, char* argv[]) {
+	char *line = NULL;
+	size_t len = 0;
+	int x, y;
+	int* grid = calloc(GRIDSIZE * GRIDSIZE, sizeof(int));
+	while (getline(&line, &len, stdin) != -1) {
+		int idx = 0;
+		int x0, y0, x1, y1;
+		enum command cmd = ON;
+		// parse command
+		if (!strncmp(line, "turn on", 7)) {
+			idx = 8;
+			cmd = ON;
+		}
+		else if (!strncmp(line, "turn off", 8)) {
+			idx = 9;
+			cmd = OFF;
+		}
+		else if (!strncmp(line, "toggle", 6)) {
+			idx = 7;
+			cmd = TOGGLE;
+		}
+		else {
+			fprintf(stderr, "unknown cmd %s\n", line);
+			return 1;
+		}
+		parse_coordinate(line, &idx, &x0, &y0);
+		// find next coordinate start
+		while (line[idx] < '0' || line[idx] > '9')
+			++idx;
+		parse_coordinate(line, &idx, &x1, &y1);
+		for (x = x0; x <= x1; ++x) {
+			for (y = y0; y <= y1; ++y) {
+				int grididx = x + y * GRIDSIZE;
+				grid[grididx] += cmd == ON ? 1 : (cmd == OFF ? -1 : 2);
+				if (grid[grididx] < 0)
+					grid[grididx] = 0;
+			}
+		}
+	}
+	free(line);
+
+	int total = 0;
+	for (x = 0; x < GRIDSIZE; ++x) {
+		for (y = 0; y < GRIDSIZE; ++y) {
+			total += grid[x + y * GRIDSIZE];
+		}
+	}
+	
+	printf("%d\n", total);
+	return 0;
+}
