02467a6e6bb8032aa66d2b781b21b56483c1c09e
Kelly was Here <kellywashere@yahoo.com>
2021-12-04 11:17:57 +0100

    Solved 08A

---
+++ 2015/08A.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define GRIDSIZE 1000
+
+int count_delta(const char* str) {
+	int count = 0;
+	while (*str != '"')
+		++str;
+	++str; // skip "
+	++count; // and count it
+	while (*str != '"') {
+		if (*str == '\\') {
+			++str;
+			switch (*str) {
+				case '"':
+				case '\\':
+					++count;
+					break;
+				case 'x':
+					count += 3;
+					str += 2;
+					break;
+				default:
+					fprintf(stderr, "Unexpected char %c\n", *str);
+			}
+		}
+		++str;
+	}
+	++count; // last "
+	return count;
+}
+
+int main(int argc, char* argv[]) {
+	char *line = NULL;
+	size_t len = 0;
+	int count = 0;
+	while (getline(&line, &len, stdin) != -1) {
+		count += count_delta(line);
+	}
+	free(line);
+	printf("%d\n", count);
+
+	return 0;
+}
