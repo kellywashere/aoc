fc48e26283f1cd84868107508565ce3118d5fdb7
Kelly was Here <kellywashere@yahoo.com>
2021-12-04 14:30:48 +0100

    Solved 08B

---
+++ 2015/08B.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define GRIDSIZE 1000
+
+int count_delta(const char* str) {
+	int count = 2; // surrounding quotes
+	while (*str) {
+		if (*str == '"' || *str == '\\')
+			++count;
+		++str;
+	}
+	return count;
+}
+
+int main(int argc, char* argv[]) {
+	char *line = NULL;
+	size_t len = 0;
+	int count = 0;
+	while (getline(&line, &len, stdin) != -1) {
+		count += count_delta(line);
+	}
+	free(line);
+	printf("%d\n", count);
+
+	return 0;
+}
