d7eb8650f9c574f8640c0a4da09fb46178524e8b
Kelly was Here <kellywashere@yahoo.com>
2021-12-11 15:08:24 +0100

    Solved 2015/19A

--- 2015/19A_debug.c
+++ 2015/19A_debug.c
@@ -1,3 +1,4 @@
+/* Brute force method to at least produce the answer for reference */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -76,10 +77,24 @@ struct rule* process_replacement(char* line, char* atoms[]) {
 	t = strtok(NULL, delim);
 	rule->m = molecule_long_to_short_name(t, atoms);
 
+	printf("%c -> %s\n", rule->a, rule->m);
 	return rule;
 }
 
-bool check_duplicate(char* m, int idx, struct rule* r, struct rule* rules) {
+// 0 1   2    3 4 5      lm = 6, idx = 2
+// 0 1 2a 2b 2c 3 4 5    lr = 3
+char* make_replacement(char* m, int idx, struct rule* r) {
+	int lr = strlen(r->m);
+	int lm = strlen(m);
+	char* newm = malloc((lm + lr) * sizeof(char));
+	if (idx > 0)
+		memcpy(newm, m, idx);
+	memcpy(newm + idx, r->m, lr);
+	memcpy(newm + idx + lr, m + idx + 1, lm - idx); // also copies \0
+	return newm;
+}
+
+bool check_duplicate(char* m, int idx, struct rule* r, struct rule* rules, bool verbose) {
 	int lr = strlen(r->m); // length of replacement
 	if (lr == 1)
 		return false; // replacements of len 1 are always unique
@@ -108,8 +123,14 @@ bool check_duplicate(char* m, int idx, struct rule* r, struct rule* rules) {
 				//check overlap m and r->m
 				for (int jj = 1; equal && jj <= dist - 1 && lr - jj - 1 >= 0; ++jj)
 					equal = equal && m[idx - jj] == r->m[lr - jj - 1];
-				if (equal)  // found replication
+				if (equal)  { // found replication
+					if (verbose) {
+						printf("relevant substring: %.*s\n", dist + 1, m + idx - dist);
+						printf("Rule for first char: %c => %s\n", rp->a, rp->m);
+						printf("Rule for last char:  %c => %s\n", r->a, r->m);
+					}
 					return true;
+				}
 			}
 			rp = rp->next;
 		}
@@ -117,25 +138,60 @@ bool check_duplicate(char* m, int idx, struct rule* r, struct rule* rules) {
 	return false;
 }
 
+struct m_list {
+	char* m;
+	struct m_list* next;
+};
+
 int process_calmolecule(char* line, char* atoms[], struct rule* rules) {
-	int count = 0;
+	struct m_list* m_list = NULL;
+	struct m_list* e;
 	char* m = molecule_long_to_short_name(line, atoms);
+	printf("%s\n", m);
 	int lm = strlen(m);
-	// we go char by char over m. We check if replacement of the char under consideration
-	// leads to a string that could have been achieved by an earlier replacement
 
 	for (int ii = 0; ii < lm; ++ii) {
-		char a = m[ii];
 		struct rule* r = rules;
 		while (r) {
-			if (r->a == a) { // found replacement rule
-				if (!check_duplicate(m, ii, r, rules))
-					++count;
+			if (r->a == m[ii]) { // found replacement rule
+				char* newm = make_replacement(m, ii, r);
+				// check for presence in list
+				e = m_list;
+				bool is_duplicate = check_duplicate(m, ii, r, rules, false);
+				bool found = false;
+				while (!found && e) {
+					if (!strcmp(newm, e->m)) {
+						if (!is_duplicate)
+							printf("\ncheck_duplicate missed one:\n%s @ %d\n", m, ii);
+						free(newm);
+						found = true;
+					}
+					e = e->next;
+				}
+				if (!found) { // cons
+					if (is_duplicate) {
+						printf("\ncheck_duplicate: false positive:\n%s @ %d\n", m, ii);
+						check_duplicate(m, ii, r, rules, true);
+					}
+					e = malloc(sizeof(m_list));
+					e->m = newm;
+					e->next = m_list;
+					m_list = e;
+				}
 			}
 			r = r->next;
 		}
 	}
 
+	// clean up, and count
+	int count = 0;
+	while (m_list) {
+		e = m_list;
+		m_list = m_list->next;
+		free(e->m);
+		free(e);
+		++count;
+	}
 	free(m);
 	return count;
 }
