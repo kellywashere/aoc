5b47dd3be74cecc74d77026c878ed2d737aa6d62
Kelly was Here <kellywashere@yahoo.com>
2021-12-23 08:39:55 +0100

    Still working on 2021/22B

--- 2021/22B.c
+++ 2021/22B.c
@@ -26,6 +26,17 @@ struct cube {
 	struct cube* next;
 };
 
+struct cube* create_cube(int x0, int x1, int y0, int y1, int z0, int z1) {
+	struct cube* c = mymalloc(sizeof(struct cube));
+	c->x[0] = x0;
+	c->x[1] = x1;
+	c->y[0] = y0;
+	c->y[1] = y1;
+	c->z[0] = z0;
+	c->z[1] = z1;
+	return c;
+}
+
 bool intersect(struct cube* c1, struct cube* c2) {
 	if (c2->x[1] < c1->x[0] || (c2->x[0] > c1->x[1]))
 		return false;
@@ -69,18 +80,86 @@ void process_line(char* line, struct cube** cubes) {
 		return;
 
 	struct cube* c = *cubes;
+	struct cube* prev = NULL;
 	while (c) {
 		if (intersect(c, cube)) {
-			if (turnon)
-				turn_on(c, cube);
-			else
-				turn_off(c, cube);
+			// chop of off pieces of c that do not intersect with cube
+			if (cube->x[0] > c->x[0]) {
+				// split c into s (no overap with cube) and c (overlap)
+				struct cube* s = create_cube(c->x[0], cube->x[0] - 1, c->y[0], c->y[1], c->z[0], c->z[1]);
+				c->x[0] = cube->x[0];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			if (cube->x[1] < c->x[1]) {
+				struct cube* s = create_cube(cube->x[1] + 1, c->x[1], c->y[0], c->y[1], c->z[0], c->z[1]);
+				c->x[1] = cube->x[1];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			if (cube->y[0] > c->y[0]) {
+				struct cube* s = create_cube(c->x[0], c->x[1], c->y[0], cube->y[0] - 1, c->z[0], c->z[1]);
+				c->y[0] = cube->y[0];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			if (cube->y[1] < c->y[1]) {
+				struct cube* s = create_cube(c->x[0], c->x[1], cube->y[1] + 1, c->y[1], c->z[0], c->z[1]);
+				c->y[1] = cube->y[1];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			if (cube->z[0] > c->z[0]) {
+				struct cube* s = create_cube(c->x[0], c->x[1], c->y[0], c->y[1], c->z[0], cube->z[0] - 1);
+				c->z[0] = cube->z[0];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			if (cube->z[1] < c->z[1]) {
+				struct cube* s = create_cube(c->x[0], c->x[1], c->y[0], c->y[1], cube->z[1] + 1, c->z[1]);
+				c->z[1] = cube->z[1];
+				s->next = *cubes;
+				*cubes = s;
+				if (!prev) prev = s;
+			}
+			// remove c from list
+			if (!prev) { // only happens when first cube on list is subset of second one
+				*cubes = c->next;
+				free(c);
+				c = *cubes; // next iter
+			}
+			else {
+				prev->next = c->next;
+				free(c);
+				c = prev->next;
+			}
 		}
-		c = c->next;
+		else {
+			prev = c;
+			c = c->next;
+		}
 	}
-	// add cube to front of list
-	cube->next = *cubes;
-	*cubes = cube;
+	if (turnon) {
+		cube->next = *cubes;
+		*cubes = cube;
+	}
+}
+
+int count_cubes(struct cube* cubes) {
+	int count = 0;
+	while (cubes) {
+		int x = cubes->x[1] - cubes->x[0] + 1;
+		int y = cubes->y[1] - cubes->y[0] + 1;
+		int z = cubes->z[1] - cubes->z[0] + 1;
+		count += x * y * z;
+		cubes = cubes->next;
+	}
+	return count;
 }
 
 int main(int argc, char* argv[]) {
@@ -91,6 +170,14 @@ int main(int argc, char* argv[]) {
 		if (empty_line(line))
 			continue;
 		process_line(line, &cubes);
+	}
+	printf("%d\n", count_cubes(cubes));
+	// clean up
+	while (cubes) {
+		struct cube* c = cubes;
+		cubes = c->next;
+		// print_cube(c);
+		free(c);
 	}
 	free(line);
 	return 0;
