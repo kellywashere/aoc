6ba947f7da49a0c405a785ca1a8071b1a8733ed6
Kelly was Here <kellywashere@yahoo.com>
2022-05-14 22:33:28 +0200

    Simpler version of 2016/13A, without using minheap as set

--- 2016/13A_simpler.c
+++ 2016/13A_simpler.c
@@ -7,67 +7,55 @@
 
 /************************* MINHEAP *******************************/
 struct minheap_el {
-	int          key;
-	unsigned int val;
+	int key;
+	int val;
 };
 
-struct minheap_vallut { // minheap, with lut for val-->idx translation to find val in heap with O(1)
-	// NOTE: this structure is a set as far as val is concerned. One value can appear only once in structure
+struct minheap {
 	struct minheap_el* mem;      // array-based min heap
 	unsigned int       size;     // nr elements currently in heap
 	unsigned int       capacity; // capacity in nr of elements
-	unsigned int       maxval;   // maximum value of val  TODO: hash table does not need this
-	unsigned int*      vallut;   // vallut[val] gives idx in mem where this val is stored  TODO: hash table
 };
 
-struct minheap_vallut* create_minheap_vallut(unsigned int maxval) {
-	struct minheap_vallut* heap = malloc(sizeof(struct minheap_vallut));
+struct minheap* create_minheap() {
+	struct minheap* heap = malloc(sizeof(struct minheap));
 	heap->capacity = 127; // 7 layers start size
 	heap->mem = malloc(sizeof(struct minheap_el) * heap->capacity);
 	heap->size = 0;
-	heap->maxval = maxval;
-	heap->vallut = malloc((maxval + 1) * sizeof(int));
-	for (unsigned int ii = 0; ii <= maxval; ++ii)
-		heap->vallut[ii] = -1;
 	return heap;
 }
 
-void destroy_minheap_vallut(struct minheap_vallut* heap) {
-	if (heap) {
+void destroy_minheap(struct minheap* heap) {
+	if (heap)
 		free(heap->mem);
-		free(heap->vallut);
-		free(heap);
-	}
+	free(heap);
 }
 
-static void minheap_vallut_swap(struct minheap_vallut* heap, int idx1, int idx2) {
+static void minheap_swap(struct minheap* heap, int idx1, int idx2) {
 	struct minheap_el t = heap->mem[idx1];
 	heap->mem[idx1] = heap->mem[idx2];
 	heap->mem[idx2] = t;
-	heap->vallut[heap->mem[idx1].val] = idx1;
-	heap->vallut[heap->mem[idx2].val] = idx2;
 }
 
-static unsigned int minheap_vallut_bubble_up(struct minheap_vallut* heap, unsigned int idx) {
-	// returns new idx of bubbled-up item
-	unsigned int idx_parent = (idx - 1) / 2;
+static void minheap_bubble_up(struct minheap* heap, int idx) {
+	int idx_parent = (idx - 1) / 2;
 	while (idx != 0 && heap->mem[idx].key < heap->mem[idx_parent].key) {
-		minheap_vallut_swap(heap, idx, idx_parent);
+		minheap_swap(heap, idx, idx_parent);
 		idx = idx_parent;
 		idx_parent = (idx - 1) / 2;
 	}
-	return idx;
 }
 
-static unsigned int minheap_vallut_bubble_down(struct minheap_vallut* heap, unsigned int idx) {
+static void minheap_bubble_down(struct minheap* heap) {
+	int idx = 0;
 	int key_this = heap->mem[idx].key;
 	bool done = false;
 	while (!done) {
 		done = true;
 		int smallest_key = key_this;
-		unsigned int idx_smallest = idx;
+		int idx_smallest = idx;
 		// find smallest between idx, left and right child
-		unsigned int idx_child;
+		int idx_child;
 		for (idx_child = (idx * 2) + 1; idx_child <= (idx * 2) + 2; ++idx_child) {
 			if (idx_child < heap->size) {
 				int k = heap->mem[idx_child].key;
@@ -79,33 +67,27 @@ static unsigned int minheap_vallut_bubble_down(struct minheap_vallut* heap, unsi
 		}
 		if (idx_smallest != idx) {
 			done = false;
-			minheap_vallut_swap(heap, idx, idx_smallest);
+			minheap_swap(heap, idx, idx_smallest);
 			idx = idx_smallest;
 		}
 	}
-	return idx;
 }
 
-void minheap_vallut_insert(struct minheap_vallut* heap, int key, unsigned int val) {
-	if (heap->vallut[val] != -1) {
-		fprintf(stderr, "Error: value %d already appears in heap\n", val);
-		exit(1);
-	}
+void minheap_insert(struct minheap* heap, int key, int val) {
 	// check capacity
 	if (heap->size == heap->capacity) {
 		heap->capacity = 2 * heap->capacity + 1; // 1 more layer
 		heap->mem = realloc(heap->mem, sizeof(struct minheap_el) * heap->capacity);
 	}
-	unsigned int idx = heap->size; // insertion idx
+	int idx = heap->size; // insertion idx
 	// copy info into new last element
 	heap->mem[idx].key = key;
 	heap->mem[idx].val = val;
-	heap->vallut[val] = idx;
 	++heap->size;
-	minheap_vallut_bubble_up(heap, idx);
+	minheap_bubble_up(heap, idx);
 }
 
-int minheap_vallut_extract(struct minheap_vallut* heap, unsigned int* val) { // returns key, copies val to *val
+int minheap_extract(struct minheap* heap, int* val) { // returns key, copies val to *val
 	if (heap->size == 0) {
 		fprintf(stderr, "Error: extracting from empty heap\n");
 		exit(1);
@@ -113,31 +95,14 @@ int minheap_vallut_extract(struct minheap_vallut* heap, unsigned int* val) { // 
 	int retKey = heap->mem[0].key;
 	if (val)
 		*val = heap->mem[0].val;
-	heap->vallut[heap->mem[0].val] = -1;
 
 	--heap->size;
 	if (heap->size > 0) {
 		// copy ex-last element to root position
 		heap->mem[0] = heap->mem[heap->size];
-		heap->vallut[heap->mem[0].val] = 0;
-		minheap_vallut_bubble_down(heap, 0);
+		minheap_bubble_down(heap);
 	}
 	return retKey;
-}
-
-bool minheap_vallut_hasvalue(struct minheap_vallut* heap, unsigned int val) {
-	return heap->vallut[val] != -1;
-}
-
-void minheap_vallut_changekey(struct minheap_vallut* heap, int key, unsigned int val) {
-	unsigned int idx = heap->vallut[val];
-	if (idx == -1) {
-		fprintf(stderr, "Error: value %d does not appear in heap\n", val);
-		exit(1);
-	}
-	heap->mem[idx].key = key;
-	idx = minheap_vallut_bubble_up(heap, idx);
-	minheap_vallut_bubble_down(heap, idx);
 }
 /************************* MINHEAP *******************************/
 
@@ -205,10 +170,10 @@ int dir_to_didx(enum dir dir, int gridw) {
 }
 
 #ifdef SHOW_PATH
-void show_path(int startx, int starty, int targetx, int targety, bool* grid, unsigned int* camefrom, int gridw, int gridh) {
+void show_path(int startx, int starty, int targetx, int targety, bool* grid, int* camefrom, int gridw, int gridh) {
 	int x, y;
-	unsigned int idx;
-	unsigned int startidx = starty * gridw + startx;
+	int idx;
+	int startidx = starty * gridw + startx;
 
 	int* solgrid = malloc(gridw * gridh * sizeof(int));
 	for (idx = 0; idx < gridw * gridh; ++idx)
@@ -216,7 +181,7 @@ void show_path(int startx, int starty, int targetx, int targety, bool* grid, uns
 	// backtrace from target to start
 	idx = targety * gridw + targetx;
 	while (idx != startidx) {
-		unsigned int nextidx = camefrom[idx];
+		int nextidx = camefrom[idx];
 		solgrid[nextidx] = didx_to_dir(idx - nextidx);
 		idx = nextidx;
 	}
@@ -240,7 +205,7 @@ void show_path(int startx, int starty, int targetx, int targety, bool* grid, uns
 
 void find_path(int startx, int starty, int targetx, int targety, int key) {
 	int x, y;
-	unsigned int idx;
+	int idx;
 
 	// init
 	int gridw = 2 * targetx;
@@ -250,7 +215,7 @@ void find_path(int startx, int starty, int targetx, int targety, int key) {
 	bool* grid = malloc(gridw * gridh * sizeof(bool));
 	int* gScore = malloc(gridw * gridh * sizeof(int));
 #ifdef SHOW_PATH
-	unsigned int* camefrom = malloc(gridw * gridh * sizeof(unsigned int));
+	int* camefrom = malloc(gridw * gridh * sizeof(int));
 #endif
 	for (x = 0; x < gridw; ++x) {
 		for (y = 0; y < gridh; ++y) {
@@ -259,19 +224,19 @@ void find_path(int startx, int starty, int targetx, int targety, int key) {
 			gScore[idx] = INT_MAX;
 		}
 	}
-	struct minheap_vallut* heap = create_minheap_vallut(gridw * gridh - 1);
+	struct minheap* heap = create_minheap();
 
 	x = startx;
 	y = starty;
 	idx = y * gridw + x;
 	gScore[idx] = 0;
 	int fScore = hScore(x, y, targetx, targety);
-	minheap_vallut_insert(heap, fScore, idx);
+	minheap_insert(heap, fScore, idx);
 
 	bool found = false;
 	while (heap->size) {
 		// extract node with lowest fScore
-		minheap_vallut_extract(heap, &idx); // idx = cur
+		minheap_extract(heap, &idx); // idx = cur
 		if (idx == targetidx) {
 			found = true;
 			break;
@@ -287,10 +252,7 @@ void find_path(int startx, int starty, int targetx, int targety, int key) {
 					gScore[nidx] = tentative_gScore;
 					int h = hScore(nidx % gridw, nidx / gridw, targetx, targety);
 					fScore = tentative_gScore + h;
-					if (minheap_vallut_hasvalue(heap, nidx))
-						minheap_vallut_changekey(heap, fScore, nidx);
-					else
-						minheap_vallut_insert(heap, fScore, nidx);
+					minheap_insert(heap, fScore, nidx);
 				}
 			}
 		}
@@ -307,7 +269,7 @@ void find_path(int startx, int starty, int targetx, int targety, int key) {
 #ifdef SHOW_PATH
 	free(camefrom);
 #endif
-	destroy_minheap_vallut(heap);
+	destroy_minheap(heap);
 }
 
 int main(int argc, char* argv[]) {
