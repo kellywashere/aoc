3c46081c27721f103035b7b16c7d46f4d005c320
Remco van de Beek <remco.van.de.beek@nxp.com>
2022-05-27 16:52:39 +0200

    Solved 2017/05

---
+++ 2017/05A.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct int_array {
+	int* data;
+	int  size;
+	int  capacity;
+};
+
+struct int_array* create_int_array() {
+	struct int_array* a = malloc(sizeof(struct int_array));
+	a->size = 0;
+	a->capacity = 256;
+	a->data = malloc(a->capacity * sizeof(int));
+	return a;
+}
+
+void destroy_int_array(struct int_array* a) {
+	if (a) {
+		free(a->data);
+		free(a);
+	}
+}
+
+void int_array_add(struct int_array* a, int x) {
+	if (a->size + 1 >= a->capacity) {
+		a->capacity *= 2;
+		a->data = realloc(a->data, a->capacity * sizeof(int));
+	}
+	a->data[a->size++] = x;
+}
+
+int simulate(struct int_array* jmps) {
+	int steps = 0;
+	int pc = 0;
+	while (pc >= 0 && pc < jmps->size) {
+		int j = jmps->data[pc];
+		jmps->data[pc]++;
+		pc = pc + j;
+		++steps;
+	}
+	return steps;
+}
+
+int main(int argc, char* argv[]) {
+	int x;
+	struct int_array* arr = create_int_array();
+	while (scanf("%d", &x) == 1)
+		int_array_add(arr, x);
+	int steps = simulate(arr);
+	printf("%d\n", steps);
+	destroy_int_array(arr);
+	return 0;
+}
